5226cb0f36955cdae48b1273ae0a4c29c6ea6739
Ahammed Fahim Galib <galib0124@gmail.com>
2025-01-27 00:27:51 +0600

    cors added

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ async function booststrap() {
         console.log("database is connected successfully");
 
         app.listen(config.port, () => {
-            console.log(`Example app listening on port ${config.port}`)
+            console.log(`App listening on port ${config.port}`)
           })
     } catch (err){
         console.log(`failed to connect data base ${err}`);
